204044628449241b018ba92e43bf007490620339
AndresTMX <andresft437@gmail.com>
2026-03-28 12:07:42 -0600

    feat: update conf

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -71,6 +71,10 @@ export default defineConfig({
           enable: '/api/draft-mode/enable',
         },
       },
+      allowOrigins: [
+        'https://www.femestructural.com.mx',
+        'https://femestructural.com.mx',
+      ],
       resolve: {
         // The Main Document Resolver API provides a method of resolving a main document from a given route or route pattern. https://www.sanity.io/docs/visual-editing/presentation-resolver-api#57720a5678d9
         mainDocuments: defineDocuments([
